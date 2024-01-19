77ff4290e7504581710ba958970667ba970d4225
Crowdbotics <team@crowdbotics.com>
2024-01-19 22:30:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mobile-recovery-cli-45454",
+  url: "https://mobile-recovery-cli-45454.botics.co",
+  api: "https://mobile-recovery-cli-45454.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
